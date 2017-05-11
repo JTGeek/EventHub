eeac64c1210cf5c133a1735d55c80dad150a59e2
James Timberlake <James.Timberlake.1018@gmail.com>
2017-05-10 21:51:46 -0400

    new commit

--- js/app.js
+++ js/app.js
@@ -34,8 +34,32 @@ function showPosition(position) {
 function showEvents(json) {
     for (var i = 0; i < json.page.size; i++) {
         $("#events").append("<p>" + json._embedded.events[i].name + "</p>");
-        //make clickable links to access ticket info from api
+        var items = $('#events .list-group-item');
+        items.hide();
+        var events = json._embedded.events;
+        var item = items.first();
+        for (var i = 0; i < events.length; i++) {
+            item.children('.list-group-item-heading').text(events[i].name);
+            item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+            try {
+                item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+            } catch (err) {
+                console.log(err);
+            }
+            item.show();
+            item.off("click");
+            item.click(events[i], function (eventObject) {
+                console.log(eventObject.data);
+                try {
+                    getAttraction(eventObject.data._embedded.attractions[0].id);
+                } catch (err) {
+                    console.log(err);
+                }
+            });
+            item = item.next();
+        }
     }
+}
 }
 
 
