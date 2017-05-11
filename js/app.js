607ad18390c1094c7e583e727e03358a472f641e
James Timberlake <James.Timberlake.1018@gmail.com>
2017-05-11 01:09:43 -0400

    new commit

--- js/app.js
+++ js/app.js
@@ -33,33 +33,10 @@ function showPosition(position) {
 
 function showEvents(json) {
     for (var i = 0; i < json.page.size; i++) {
-        $("#events").append("<p>" + json._embedded.events[i].name + "</p>");
-        var items = $('#events .list-group-item');
-        items.hide();
-        var events = json._embedded.events;
-        var item = items.first();
-        for (var i = 0; i < events.length; i++) {
-            item.children('.list-group-item-heading').text(events[i].name);
-            item.children('.list-group-item-text').text(events[i].dates.start.localDate);
-            try {
-                item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
-            } catch (err) {
-                console.log(err);
-            }
-            item.show();
-            item.off("click");
-            item.click(events[i], function (eventObject) {
-                console.log(eventObject.data);
-                try {
-                    getAttraction(eventObject.data._embedded.attractions[0].id);
-                } catch (err) {
-                    console.log(err);
-                }
-            });
-            item = item.next();
-        }
+        $("#events").append("<p><a href='" + json._embedded.events[i].url + "'>" + json._embedded.events[i].name + "</a></p>");
+        //make clickable links to access ticket info from api
+        //  ._embedded.events["0"].url
     }
-}
 }
 
 
@@ -78,12 +55,27 @@ function initMap(position, json) {
 }
 
 function addMarker(map, event) {
+    //build template for infowindow with venue, artist, and pic
+    var contentString = "";
+
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
         map: map
     });
+
+
     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
     console.log(marker);
+
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
 }
 
 
